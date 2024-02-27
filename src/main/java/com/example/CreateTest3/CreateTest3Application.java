fade1afd79ee0de9335df71482668300ef540768
= <dev.child.se@gmail.com>
2024-02-27 17:37:22 +0900

    first

---
+++ src/main/java/com/example/CreateTest3/CreateTest3Application.java
@@ -0,0 +1,13 @@
+package com.example.CreateTest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateTest3Application.class, args);
+	}
+
+}
